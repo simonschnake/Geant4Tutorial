505e0508ba2dab9a237249e95a888e92a4383f6b
Hartmut Stadie <hartmut.stadie@desy.de>
2018-04-26 18:08:14 +0200

    update toycal

--- TutorialApplication/geometry/toyCalorimeter.C
+++ TutorialApplication/geometry/toyCalorimeter.C
@@ -42,63 +42,66 @@ void toyCalorimeter()
   //construct layer
 
   // layer 0
-  Double_t width_layer0 = w_scint_0+w_steel_0;
-  TGeoVolume* layer0 = gGeoManager->MakeBox("Layer0", medVac, width_layer0, std_length, std_length);
-  layer0->AddNode(scint0, 0, new TGeoTranslation(-w_scint_0*0.5, 0, 0));
-  layer0->AddNode(steel0, 1, new TGeoTranslation(w_steel_0*0.5, 0, 0));
+  Double_t w_layer0 = w_scint_0+w_steel_0;
+  TGeoVolume* layer0 = gGeoManager->MakeBox("Layer0", medVac, w_layer0, std_length, std_length);
+  layer0->AddNode(scint0, 0, new TGeoTranslation(-w_steel_0, 0, 0));
+  layer0->AddNode(steel0, 1, new TGeoTranslation(w_scint_0, 0, 0));
 
 
   // layer 1 - 8
-  Double_t width_layer1 =   w_scint_1+w_brass_1;
-  TGeoVolume* layer1 = gGeoManager->MakeBox("Layer1", medVac, width_layer1, std_length, std_length);
-  layer1->AddNode(scint1, 0, new TGeoTranslation(-w_scint_1*0.5,0,0));
-  layer1->AddNode(brass1, 1, new TGeoTranslation(w_brass_1*0.5,0,0));
+  Double_t w_layer1 =   w_scint_1+w_brass_1;
+  TGeoVolume* layer1 = gGeoManager->MakeBox("Layer1", medVac, w_layer1, std_length, std_length);
+  layer1->AddNode(scint1, 0, new TGeoTranslation(-w_brass_1,0,0));
+  layer1->AddNode(brass1, 1, new TGeoTranslation(w_scint_1,0,0));
 
   // layer 9 - 14
-  Double_t width_layer2 = w_scint_1+w_brass_2;
-  TGeoVolume* layer2 = gGeoManager->MakeBox("Layer2", medVac, width_layer2, std_length, std_length);
-  layer2->AddNode(scint1, 0, new TGeoTranslation(-w_scint_1*0.5,0,0));
-  layer2->AddNode(brass2, 1, new TGeoTranslation(w_brass_2*0.5,0,0));
+  Double_t w_layer2 = w_scint_1+w_brass_2;
+  TGeoVolume* layer2 = gGeoManager->MakeBox("Layer2", medVac, w_layer2, std_length, std_length);
+  layer2->AddNode(scint1, 0, new TGeoTranslation(-w_brass_1,0,0));
+  layer2->AddNode(brass2, 1, new TGeoTranslation(w_scint_1,0,0));
    
   // layer 15 - 16
-  Double_t width_layer3 = w_scint_1+w_steel_3+w_scint_0;
+  Double_t w_layer3 = w_scint_1+w_steel_3+w_scint_0;
   TGeoVolume* layer3
-      = gGeoManager->MakeBox("Layer3", medVac, width_layer3, std_length, std_length);
-  layer3->AddNode(scint1, 0, new TGeoTranslation(-w_scint_1*0.5,0,0));
-  layer3->AddNode(steel3, 1, new TGeoTranslation(w_steel_3*0.5,0,0));
-  layer3->AddNode(scint0, 2, new TGeoTranslation(w_steel_3+w_scint_0*0.5,0,0));
+      = gGeoManager->MakeBox("Layer3", medVac, w_layer3, std_length, std_length);
+  layer3->AddNode(scint1, 0, new TGeoTranslation(-w_steel_3-w_scint_0,0,0));
+  layer3->AddNode(steel3, 1, new TGeoTranslation(w_scint_1-w_scint_0,0,0));
+  layer3->AddNode(scint0, 2, new TGeoTranslation(w_scint_1+w_steel_3,0,0));
 
 
   // total width
-  Double_t width_total = width_layer0+8*width_layer1+6*width_layer2+width_layer3;
+  Double_t w_total = w_layer0+8*w_layer1+6*w_layer2+w_layer3;
   
   // experimental hall, "world volume" 
-  TGeoVolume* top = gGeoManager->MakeBox("EXPH",medVac, 1.5*width_total, std_length, std_length);
+  TGeoVolume* top = gGeoManager->MakeBox("EXPH",medVac, w_total, std_length, std_length);
 
   
   // Calorimeter 
-  //  TGeoVolume* cal = gGeoManager->MakeBox("CALO",medVac, width_t
-                                         otal, std_length, std_length);
+  //  TGeoVolume* cal = gGeoManager->MakeBox("CALO",medVac, w_total, std_length, std_length);
   gGeoManager->SetTopVolume(top );  
   
   // add layers to top
-  Double_t x = -width_total/2;
-    
+  Double_t x = -w_total;
+
+  x+= w_layer0;
   top->AddNode(layer0, 0, new TGeoTranslation(x,0,0));
-  x+= width_layer0;
+  x+= w_layer0;
 
   for(int i = 1; i <= 8; ++i)
   {
+    x+= w_layer1;
     top->AddNode(layer1, i, new TGeoTranslation(x,0,0));
-  x+= width_layer1;
+    x+= w_layer1;
   }
 
   for(int i = 9; i <= 14; ++i)
   {
+    x+= w_layer2;
     top->AddNode(layer2, i, new TGeoTranslation(x,0,0));
-  x+= width_layer1;
+    x+= w_layer2;
   }
 
+  x+= w_layer3;
   top->AddNode(layer3, 15, new TGeoTranslation(x,0,0));
 
   //  TGeoVolume* calo_z = calo->Divide("calo_p",3,-1,0,std_length/4,0,"S");
